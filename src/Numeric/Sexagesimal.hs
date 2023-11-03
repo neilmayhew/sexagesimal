e111e65d7f2a696c29436f937abe2a8eca74af7f
Neil Mayhew <neil@mayhew.name>
2023-11-03 17:58:01 -0600

    Implement Show fully

--- src/Numeric/Sexagesimal.hs
+++ src/Numeric/Sexagesimal.hs
@@ -1,10 +1,24 @@
 module Numeric.Sexagesimal where
 
+import Data.List (intercalate)
+import Text.Printf (printf)
+
 newtype Sexagesimal = Sexagesimal Rational
   deriving (Eq, Ord, Enum, Num, Fractional, Real, RealFrac)
 
 instance Show Sexagesimal where
-  show (Sexagesimal r) = show (round r :: Integer)
+  show (Sexagesimal r) = showSexagesimal r
+
+showSexagesimal :: Rational -> String
+showSexagesimal = intercalate ":" . zipWith ($) (show : repeat showPadded) . asSexagesimalDigits
+ where showPadded = printf "%02d"
+
+asSexagesimalDigits :: Rational -> [Int]
+asSexagesimalDigits 0 = [0]
+asSexagesimalDigits s = go s where
+  go 0 = []
+  go x = i : go (abs f * 60)
+    where (i, f) = properFraction x
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
--- test/Main.hs
+++ test/Main.hs
@@ -8,6 +8,14 @@ main = hspec spec
 
 spec :: Spec
 spec = describe "Sexagesimal" $ do
-  describe "Show" $
-    it "formats a small integer correctly" $
+  describe "Show" $ do
+    it "formats a small positive integer correctly" $
       show (42 :: Sexagesimal) `shouldBe` "42"
+    it "formats a small negative integer correctly" $
+      show (-42 :: Sexagesimal) `shouldBe` "-42"
+    it "formats pi correctly" $
+      show (realToFrac (pi :: Double) :: Sexagesimal) `shouldBe`
+        "3:08:29:44:00:47:25:53:07:23:43:33:18:45:57:12:18:17:29:34:43:53:12:11:15"
+    it "formats -pi correctly" $
+      show (realToFrac (-pi :: Double) :: Sexagesimal) `shouldBe`
+        "-3:08:29:44:00:47:25:53:07:23:43:33:18:45:57:12:18:17:29:34:43:53:12:11:15"
