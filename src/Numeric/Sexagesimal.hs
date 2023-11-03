df1fede533efbfbbfec4113a1c897b003b6c48b9
Neil Mayhew <neil@mayhew.name>
2023-11-03 17:23:06 -0600

    Define Sexagesimal

--- src/Numeric/Sexagesimal.hs
+++ src/Numeric/Sexagesimal.hs
@@ -1,4 +1,10 @@
-module Numeric.Sexagesimal (someFunc) where
+module Numeric.Sexagesimal where
+
+newtype Sexagesimal = Sexagesimal Rational
+  deriving (Eq, Ord, Enum, Num, Fractional, Real, RealFrac)
+
+instance Show Sexagesimal where
+  show (Sexagesimal r) = show (round r :: Integer)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,13 @@
 module Main (main) where
 
+import Numeric.Sexagesimal
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = hspec spec
+
+spec :: Spec
+spec = describe "Sexagesimal" $ do
+  describe "Show" $
+    it "formats a small integer correctly" $
+      show (42 :: Sexagesimal) `shouldBe` "42"
