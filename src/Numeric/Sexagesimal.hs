877e6f5495e1e335d08a670e3292159214b3eaa9
Neil Mayhew <neil@mayhew.name>
2026-01-07 07:54:21 -0700

    Implement Read

--- src/Numeric/Sexagesimal.hs
+++ src/Numeric/Sexagesimal.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Numeric.Sexagesimal where
 
 import Data.List (intercalate)
+import Text.ParserCombinators.ReadPrec
+import Text.ParserCombinators.ReadP
 import Text.Printf (printf)
+import Text.Read
 
 newtype Sexagesimal = Sexagesimal Rational
   deriving (Eq, Ord, Enum, Num, Fractional, Real, RealFrac)
@@ -19,3 +24,20 @@ asSexagesimalDigits s = go s where
   go 0 = []
   go x = i : go (abs f * 60)
     where (i, f) = properFraction x
+
+instance Read Sexagesimal where
+  readPrec = Sexagesimal <$> lift parseSexagesimal
+
+parseSexagesimal :: ReadP Rational
+parseSexagesimal = fmap fromSexagesimalDigits $
+  (:) <$> signed <*> many (colon *> unsigned)
+ where
+  colon = char ':'
+  signed = readPrec_to_P readPrec minPrec
+  unsigned = fromEnum @Word <$> readPrec_to_P readPrec minPrec
+
+fromSexagesimalDigits :: [Int] -> Rational
+fromSexagesimalDigits [] = 0
+fromSexagesimalDigits (d : ds)
+  | d < 0 = negate $ fromSexagesimalDigits (negate d : ds)
+  | otherwise = toRational d + fromSexagesimalDigits ds / 60
--- test/Main.hs
+++ test/Main.hs
@@ -22,3 +22,16 @@ spec = describe "Sexagesimal" $ do
     it "formats -pi correctly" $
       realToFrac @Double (-pi) `shouldShow`
         "-3:08:29:44:00:47:25:53:07:23:43:33:18:45:57:12:18:17:29:34:43:53:12:11:15"
+
+  describe "Read" $ do
+    let shouldRead = shouldBe . read @Sexagesimal
+    it "parses a small positive integer correctly" $
+      "42" `shouldRead` 42
+    it "parses a small negative integer correctly" $
+      "-42" `shouldRead` (-42)
+    it "parses pi correctly" $
+      "3:08:29:44:00:47:25:53:07:23:43:33:18:45:57:12:18:17:29:34:43:53:12:11:15"
+        `shouldRead` realToFrac @Double pi
+    it "parses -pi correctly" $
+      "-3:08:29:44:00:47:25:53:07:23:43:33:18:45:57:12:18:17:29:34:43:53:12:11:15"
+        `shouldRead` realToFrac @Double (-pi)
